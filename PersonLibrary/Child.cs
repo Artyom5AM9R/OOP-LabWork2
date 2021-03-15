db2f692fffd6366ff7809bed2702b35adac9bf19
Artyom5AM9R <75558178+Artyom5AM9R@users.noreply.github.com>
2021-03-16 00:04:46 +0700

    feature(Child): добавлены основные поля

--- PersonLibrary/Child.cs
+++ PersonLibrary/Child.cs
@@ -11,5 +11,19 @@ namespace PersonLibrary
     /// </summary>
     class Child : Person
     {
+        /// <summary>
+        /// Поле для описания матери ребёнка
+        /// </summary>
+        private Person _mother;
+
+        /// <summary>
+        /// Поле для описания отца ребёнка
+        /// </summary>
+        private Person _father;
+
+        /// <summary>
+        /// Поле для описания названия детского сада/школы ребёнка
+        /// </summary>
+        private string _placeOfStudy;
     }
 }
