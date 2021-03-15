ead4fb14a6487ae502feef78f39b583738b21a8b
Artyom5AM9R <75558178+Artyom5AM9R@users.noreply.github.com>
2021-03-16 00:00:01 +0700

    feature(Child): добавлен новый класс

---
+++ PersonLibrary/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    /// <summary>
+    /// Класс для описания детей
+    /// </summary>
+    class Child : Person
+    {
+    }
+}
