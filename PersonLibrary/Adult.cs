3c4fe7e46afaa25c135cdabf7d7089d94a6192aa
Artyom5AM9R <75558178+Artyom5AM9R@users.noreply.github.com>
2021-03-15 23:33:08 +0700

    feature(Adult): добавлен новый класс

---
+++ PersonLibrary/Adult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    /// <summary>
+    /// Класс для описания взрослых людей
+    /// </summary>
+    class Adult : Person
+    {
+    }
+}
